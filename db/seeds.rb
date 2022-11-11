b87e2559d860201acb3d3a535f89c458001a40af
hilario06 <hilario.huaman.6@gmail.com>
2022-11-11 13:03:50 -0500

    added seeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category1 = Category.find_or_initialize_by(name: 'verduras')
+category1.save
+category2 = Category.find_or_initialize_by(name: 'frutas')
+category2.save
+category3 = Category.find_or_initialize_by(name: 'carnes')
+category3.save
+
+Category.all.each do |category|
+  [5,6,7,8].sample.times do
+    Product.create(
+      name:Faker::Commerce.product_name,
+      price: Faker::Commerce.price,
+      discount: (10..80).to_a.sample,
+      category: category
+    )
+  end
+end
