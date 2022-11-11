11b0ff6907190a3db38f480ffee25711668b61c4
hilario06 <hilario.huaman.6@gmail.com>
2022-11-11 13:07:29 -0500

    added config I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)', locale: /es|en/ do
+    namespace :api do
+      namespace :v1, defaults: { format: 'json' } do
+        resources :products
+      end
+    end
+  end
 end
